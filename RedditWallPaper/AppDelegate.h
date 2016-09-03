a749686edd5314ff9ea60e321b302fd8e3ecf8b2
Lakshminarayana B R <lakshminarayanabr@cpe-2606-a000-660a-ca00-9a01-a7ff-fe93-606f.dyn6.twc.com>
2016-09-03 14:14:09 -0400

    Initial Commit

---
+++ RedditWallPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditWallPaper
+//
+//  Created by Lakshminarayana B R on 9/3/16.
+//  Copyright © 2016 Lakshminarayana B R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditWallPaper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RedditWallPaper
+//
+//  Created by Lakshminarayana B R on 9/3/16.
+//  Copyright © 2016 Lakshminarayana B R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
